1dddc63ad79cff2f2e0b0d68b7afa54c6cfe8fce
dvarshanidze <davittvarshanidze@gmail.com>
2024-05-11 16:43:54 +0400

    ss

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Show from './pages/Show';
 import './style.scss'
-
+//
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
